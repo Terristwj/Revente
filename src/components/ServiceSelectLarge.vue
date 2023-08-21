9c9c34c843fad0b539f1c3c87900e066870b5a93
Terris <27739101+Terristwj@users.noreply.github.com>
2023-08-22 04:14:35 +0800

    Updated home menu items

--- src/components/ServiceSelectLarge.vue
+++ src/components/ServiceSelectLarge.vue
@@ -1,6 +1,14 @@
 <template>
     <div id="ServiceSelectLarge">
         <div class="w-full rounded-2xl bg-custom-color custom-height">
+            <div v-if="promo" class="w-full relative z-10">
+                <span
+                    class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-green-700 p-0.5 px-2 text-gray-100 rounded-3xl text-xs"
+                >
+                    Promo</span
+                >
+            </div>
+
             <div class="flex justify-between p-2">
                 <div class="text-lg ml-2 text-gray-900 mt-14">
                     {{ text }}
@@ -23,6 +31,7 @@ const props = defineProps({
     text: String,
     imageWidth: String,
     image: String,
+    promo: { Boolean, default: false },
 });
 const { text, imageWidth, image } = toRefs(props);
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,19 @@
         <div class="m-3 mb-auto">
             <div class="grid grid-cols-2 gap-3 my-3">
                 <ServiceSelectLarge text="Ride" imageWidth="74" image="ride" />
+                <ServiceSelectLarge
+                    text="Food"
+                    imageWidth="72"
+                    image="food"
+                    :promo="true"
+                />
+            </div>
+
+            <div class="grid grid-cols-4 gap-3 my-3">
+                <ServiceSelectSmall text="2 Wheels" image="2wheels" />
+                <ServiceSelectSmall text="Transit" image="transit" />
+                <ServiceSelectSmall text="Reserve" image="reserve" />
+                <ServiceSelectSmall text="Travel" image="travel" />
             </div>
         </div>
     </div>
@@ -10,4 +23,5 @@
 
 <script setup>
 import ServiceSelectLarge from "@/components/ServiceSelectLarge.vue";
+import ServiceSelectSmall from "@/components/ServiceSelectSmall.vue";
 </script>
