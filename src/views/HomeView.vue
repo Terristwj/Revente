ed0257c153b7fb4f6493104fdb8e7b0954c68b6d
Terris <27739101+Terristwj@users.noreply.github.com>
2023-08-22 03:35:15 +0800

    Tested and successfully installed TailwindCSS

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,5 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home bg-black" style="color: red">HOME</div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<script setup></script>
