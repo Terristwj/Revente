5ff3aa7a158553b8987534c36684f8a48ea4e423
adriank0h <adriankohcl01@gmail.com>
2023-10-03 12:19:17 +0800

    added login,regi,firebase pages

---
+++ src/views/LoginView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+    <div>Login Page</div>
+</template>
+
+<style scoped></style>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+    <div>Registration Page</div>
+</template>
+
+<style scoped></style>
