b0dbda22ce028750d3a922a10112853cd0852386
Owen Goh <owengoh2001@gmail.com>
2023-10-16 17:28:12 +0800

    Commited ItemCard component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script>
 import NavigationBar from "./components/NavigationBar.vue";
 import FooterComponent from "./components/FooterComponent.vue";
+import ItemCard from "./components/ItemCard.vue";
 export default {
     components: {
         NavigationBar,
         FooterComponent,
+        ItemCard,
     },
 };
 </script>
@@ -13,6 +15,7 @@ export default {
     <NavigationBar />
     <div id="content">
         <RouterView />
+        <ItemCard/>
     </div>
     <FooterComponent />
 </template>
