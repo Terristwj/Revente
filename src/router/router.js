dab0129d3c556e4160cac72a60793aceb5958e3b
Owen Goh <owengoh2001@gmail.com>
2023-10-19 23:42:42 +0800

    Added profile route

--- src/router/router.js
+++ src/router/router.js
@@ -17,71 +17,80 @@ export const isAuthenticated = () => {
 
 // Add routes here
 export const routes = [
-    {
-        path: "/",
-        name: "Home",
-        component: () => import("../views/HomeView.vue"),
+  {
+    path: "/",
+    name: "Home",
+    component: () => import("../views/HomeView.vue"),
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    icon: ["fas", "info"],
+    component: () => import("../views/AboutView.vue"),
+  },
+    
+  {
+    path: "/faq",
+    name: "FAQ",
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import("../views/FAQView.vue"),
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/LoginView.vue"),
+    props: true,
+    meta: {
+      requiresNoAuth: true,
     },
-    {
-        path: "/about",
-        name: "About",
-        // route level code-splitting
-        // this generates a separate chunk (About.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import("../views/AboutView.vue"),
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: () => import("../views/RegisterView.vue"),
+    meta: {
+      requiresNoAuth: true,
     },
-    {
-        path: "/faq",
-        name: "FAQ",
-        // route level code-splitting
-        // this generates a separate chunk (About.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import("../views/FAQView.vue"),
-    },
-    {
-        path: "/login",
-        name: "Login",
-        component: () => import("../views/LoginView.vue"),
-        props: true,
-        meta: {
-            requiresNoAuth: true,
-        },
-    },
-    {
-        path: "/register",
-        name: "Register",
-        component: () => import("../views/RegisterView.vue"),
-        meta: {
-            requiresNoAuth: true,
-        },
-    },
-    // {
-    //     path: "/game",
-    //     name: "Game",
-    //     // route level code-splitting
-    //     // this generates a separate chunk (About.[hash].js) for this route
-    //     // which is lazy-loaded when the route is visited.
-    //     component: () => import("../views/GameView.vue"),
-    //     meta: {
-    //         requiresAuth: true,
-    //     },
-    // },
-    {
-        path: "/listing",
-        name: "Listing",
-        component: () => import("../views/ListingView.vue"),
-    },
-    {
-        path: "/Cart",
-        name: "Cart",
-        component: () => import("../views/CartView.vue"),
-    },
-    {
-        // path: *
-        path: "/:catchAll(.*)",
-        name: "NotFound",
-        component: () => import("../views/404View.vue"),
-    },
+  },
+  // {
+  //     path: "/game",
+  //     name: "Game",
+  //     // route level code-splitting
+  //     // this generates a separate chunk (About.[hash].js) for this route
+  //     // which is lazy-loaded when the route is visited.
+  //     component: () => import("../views/GameView.vue"),
+  //     meta: {
+  //         requiresAuth: true,
+  //     },
+  // },
+  {
+    path: "/listing",
+    name: "Listing",
+    component: () => import("../views/ListingView.vue"),
+  },
+  {
+    path: "/Cart",
+    name: "Cart",
+    component: () => import("../views/CartView.vue"),
+    icon: ["fas", "bag-shopping"],
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: () => import("../views/ProfileView.vue"),
+    icon: ["fas", "user"],
+  },
+  {
+    // path: *
+    path: "/:catchAll(.*)",
+    name: "NotFound",
+    component: () => import("../views/404View.vue"),
+  },
 ];
 
 const router = createRouter({
