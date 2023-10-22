a0f6a211b0083fc1d82c2fd37d83bada15f9a166
viqeey <fsantoso.2022@scis.smu.edu.sg>
2023-10-22 22:44:01 +0800

    origin/catalogue push, but to main
    it is working so im pushing to main first, incase i lose the code

--- src/views/ListingView.vue
+++ src/views/ListingView.vue
@@ -1,8 +1,10 @@
 <script>
 import CollapsibleSidebar from "../components/CollapsibleSidebar.vue";
+import ItemCard from '../components/ItemCard.vue';
 export default {
     components: {
         CollapsibleSidebar,
+        ItemCard,
     },
     data () {
         return {
@@ -16,19 +18,59 @@ export default {
     methods: {
         toggleOpen: function () {
             this.cSidebar.open = !this.cSidebar.open;
+        },
 
-        }
+        sidebarOpen: function () {
+            if (this.cSidebar.open == true) {
+                return true
+            } else {
+                return false
+            }
+        },
     }
 };
 </script>
 
 <template>
     <body>
-        <CollapsibleSidebar 
-        :cSidebar="cSidebar"
-        :open="cSidebar.open"
-        @toggleOpen="toggleOpen"
-        />
+        <div class="container-fluid">
+            <div :class="sidebarOpen() ? 'row flex' : 'col col-xxl-3 col-xl-3 col-lg-3 col-md-3 col-sm-3'">
+                <div :class="sidebarOpen()? 'col col-md-3 col-lg-3 col-sm-3 col-xxl-3' : ''">
+                    <CollapsibleSidebar 
+                    :cSidebar="cSidebar"
+                    :open="cSidebar.open"
+                    @toggleOpen="toggleOpen"
+                    />
+                </div>
+
+                <div :class="sidebarOpen() ? 'col col-xxl-9 col-xl-9 col-lg-9 col-md-9 col-sm-9' : 'row flex'">
+                    <div class="container-fluid">
+                        <div class="row listings shadow-sm p-3 mb-5 bg-white rounded">
+                            <h2 class="text-center">Current Listings</h2>
+                            <div class="col-xxl-3 col-xl-4 col-lg-4 col-md-6 col pb-2 list-item">
+                                <ItemCard />
+                            </div>
+                            <div class="col-xxl-3 col-xl-4 col-lg-4 col-md-6 col pb-2 list-item">
+                                <ItemCard />
+                            </div>
+                            <div class="col-xxl-3 col-xl-4 col-lg-4 col-md-6 col pb-2 list-item">
+                                <ItemCard />
+                            </div>
+                            <div class="col-xxl-3 col-xl-4 col-lg-4 col-md-6 col pb-2 list-item">
+                                <ItemCard />
+                            </div>
+                            <div class="col-xxl-3 col-xl-4 col-lg-4 col-md-6 col pb-2 list-item">
+                                <ItemCard />
+                            </div>
+                            <div class="col-xxl-3 col-xl-4 col-lg-4 col-md-6 col pb-2 list-item">
+                                <ItemCard />
+                            </div>
+            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </template>
 
