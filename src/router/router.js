bbbc2fe4580c236079f44ee409efbeb39ac30d83
0Ogw <glen.wang2709@gmail.com>
2023-10-23 22:15:05 +0800

    initial commit

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory } from "vue-router";
-import { userStore } from "../main.js";
+import { createRouter, createWebHistory } from 'vue-router';
+import { userStore } from '../main.js';
 
-import { getAuth } from "firebase/auth";
+import { getAuth } from 'firebase/auth';
 
 // const provider = new GoogleAuthProvider();
 // provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
@@ -11,125 +11,133 @@ import { getAuth } from "firebase/auth";
 // });
 
 export const isAuthenticated = () => {
-    let auth = getAuth();
-    let user = auth.currentUser;
-    return !!user;
+	let auth = getAuth();
+	let user = auth.currentUser;
+	return !!user;
 };
 
 // Add routes here
 export const routes = [
-    // Common Pages START
-    {
-        path: "/",
-        name: "Home",
-        component: () => import("../views/HomeView.vue"),
-    },
-    {
-        path: "/about",
-        name: "About",
-        icon: ["fas", "info"],
-        component: () => import("../views/AboutView.vue"),
-    },
-    {
-        path: "/faq",
-        name: "FAQ",
-        component: () => import("../views/FAQView.vue"),
-    },
-    // Common Pages END
+	// Common Pages START
+	{
+		path: '/',
+		name: 'Home',
+		component: () => import('../views/HomeView.vue'),
+	},
+	{
+		path: '/about',
+		name: 'About',
+		icon: ['fas', 'info'],
+		component: () => import('../views/AboutView.vue'),
+	},
+	{
+		path: '/faq',
+		name: 'FAQ',
+		component: () => import('../views/FAQView.vue'),
+	},
+	// Common Pages END
 
-    // Product Catalogue START
-    {
-        path: "/listing",
-        name: "Listing",
-        component: () => import("../views/ListingView.vue"),
-    },
-    {
-        path: "/Cart",
-        name: "Cart",
-        icon: ["fas", "bag-shopping"],
-        component: () => import("../views/CartView.vue"),
-    },
-    // Product Catalogue END
+	// Product Catalogue START
+	{
+		path: '/listing',
+		name: 'Listing',
+		component: () => import('../views/ListingView.vue'),
+	},
+	{
+		path: '/Cart',
+		name: 'Cart',
+		icon: ['fas', 'bag-shopping'],
+		component: () => import('../views/CartView.vue'),
+	},
+	// Product Catalogue END
 
-    // User Profile START
-    {
-        path: "/profile",
-        name: "Profile",
-        component: () => import("../views/ProfileView.vue"),
-        meta: {
-            requiresAuth: true,
-        },
-    },
-    {
-        path: "/profile/settings",
-        name: "Settings",
-        component: () => import("../views/UserSettingsView.vue"),
-        meta: {
-            requiresAuth: true,
-        },
-    },
-    // User Profile END
+	// User Profile START
+	{
+		path: '/profile',
+		name: 'Profile',
+		component: () => import('../views/ProfileView.vue'),
+		meta: {
+			requiresAuth: true,
+		},
+	},
+	{
+		path: '/profile/settings',
+		name: 'Settings',
+		component: () => import('../views/UserSettingsView.vue'),
+		meta: {
+			requiresAuth: true,
+		},
+	},
+	// User Profile END
 
-    // Login/Register START
-    {
-        path: "/login",
-        name: "Login",
-        component: () => import("../views/LoginView.vue"),
-        meta: {
-            requiresNoAuth: true,
-        },
-    },
-    {
-        path: "/register",
-        name: "Register",
-        component: () => import("../views/RegisterView.vue"),
-        meta: {
-            requiresNoAuth: true,
-        },
-    },
-    // Login/Register END
+	// Login/Register START
+	{
+		path: '/login',
+		name: 'Login',
+		component: () => import('../views/LoginView.vue'),
+		meta: {
+			requiresNoAuth: true,
+		},
+	},
+	{
+		path: '/register',
+		name: 'Register',
+		component: () => import('../views/RegisterView.vue'),
+		meta: {
+			requiresNoAuth: true,
+		},
+	},
+	// Login/Register END
 
-    // Others START
-    {
-        // path: *
-        path: "/:catchAll(.*)",
-        name: "NotFound",
-        component: () => import("../views/404View.vue"),
-    },
-    // Others END
+	// Others START
+	{
+		// path: *
+		path: '/:catchAll(.*)',
+		name: 'NotFound',
+		component: () => import('../views/404View.vue'),
+	},
+	// Others END
 
-    // Incomplete Views goes here START
-    // upload page
-    {
-        path: "/profile/upload",
-        name: "Upload",
-        component: () => import("../views/UploadProductView.vue"),
-        meta: {
-            requiresAuth: true,
-        },
-    },
-    // Incomplete Views goes here END
+	// Incomplete Views goes here START
+	// upload page
+	{
+		path: '/profile/upload',
+		name: 'Upload',
+		component: () => import('../views/UploadProductView.vue'),
+		meta: {
+			requiresAuth: true,
+		},
+	},
+	{
+		path: '/wishlist',
+		name: 'Wishlist',
+		component: () => import('../views/WishlistView.vue'),
+		meta: {
+			requiresAuth: true,
+		},
+	},
+	// Incomplete Views goes here END
 ];
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: routes,
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: routes,
 });
 
 router.beforeEach((to, from, next) => {
-    setTimeout(function () {
-        if (to.meta.requiresAuth && !isAuthenticated()) {
-            if (userStore.getUserID()) {
-                console.log(userStore.getUserID());
-            } else {
-                router.push("/login");
-            }
-        } else if (to.meta.requiresNoAuth && isAuthenticated()) {
-            router.push("/");
-        } else {
-            next();
-        }
-    }, 1000);
+	setTimeout(function () {
+		if (to.meta.requiresAuth && !isAuthenticated()) {
+			if (userStore.getUserID()) {
+				console.log(userStore.getUserID());
+			} else {
+				router.push('/login');
+			}
+		} else if (to.meta.requiresNoAuth && isAuthenticated()) {
+			router.push('/');
+		} else {
+			next();
+		}
+	}, 1000);
 });
 
 export default router;
---
+++ src/views/WishlistView.vue
@@ -0,0 +1,19 @@
+<script>
+	export default {
+		data() {
+			return {};
+		},
+		methods: {},
+		computed: {},
+		watch: {},
+	};
+</script>
+
+<template>
+	<div class="container">
+		<h1>WISHLIST</h1>
+        
+	</div>
+</template>
+
+<style scoped></style>
