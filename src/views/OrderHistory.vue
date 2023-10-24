c920796a178240a0e8186e9bc961c0e6385f4d8f
viqeey <fsantoso.2022@scis.smu.edu.sg>
2023-10-24 16:16:56 +0800

    Update OrderHistory.vue

--- src/views/OrderHistory.vue
+++ src/views/OrderHistory.vue
@@ -1,133 +1,64 @@
 <script>
-	import CartItem from '../components/CartItem.vue';
-	import CheckoutBar from '../components/CheckoutBar.vue';
-	export default {
-		data() {
-			return {
-				cart: [
-					{
-						name: "Mens's Jeans",
-						price: 10,
-						size: 'M',
-						brand: 'Unbranded',
-						seller: 'John Doe',
-						imgUrl: '/src/assets/img/ecommerce/Google.png',
-					},
-					{
-						name: "Mens's Jeans",
-						price: 10,
-						size: 'M',
-						brand: 'Unbranded',
-						seller: 'John Doe',
-						imgUrl: '/src/assets/img/ecommerce/Google.png',
-					},
-				],
-			};
-		},
-		computed: {
-			cartContent() {
-				if (this.cart.length === 0) {
-					return false;
-				} else {
-					return this.cart.length;
-				}
-			},
-			cartTotal() {
-				let total = 0;
-				for (let i = 0; i < this.cart.length; i++) {
-					total += this.cart[i].price;
-				}
-				return total;
-			},
-		},
-		methods: {
-			cartItemStyle(index) {
-				let styling = '';
-				if (index === this.cart.length - 1) {
-					styling += 'mb-5';
-				}
-				if (index === 0) {
-					styling += ' mt-0';
-				} else {
-					styling += ' mt-5';
-				}
-				return styling;
-			},
-		},
+import PastOrders from "../components/PastOrder.vue";
+export default {
+	// <div id='app'></div>
 		components: {
-			CartItem,
-			CheckoutBar,
+			PastOrders,
 		},
-		mounted() {
-			// When enter from About page - START
-			if (sessionStorage.getItem('toReload')) {
-				sessionStorage.removeItem('toReload');
-				location.reload();
+		data() { 
+			return { 
+		// key: value
+			items: [
+				{ deliverydate: "2023/10/24", imgUrl: "/src/assets/img/ecommerce/placeholderImage.jpg", brand: "gucci", size: "L", seller: "Lay Foo", name: "Men's Jeans" },
+
+				{ deliverydate: "2023/10/22", imgUrl: '/src/assets/img/ecommerce/placeholderImage.jpg', brand: "gucci", size: "L", seller: "Lay Foo", name: "Men's Jeans" },
+			],
+			};
+		}, // data
+		// computed: { 
+		//     derivedProperty() {
+		//         return false;
+		//     }  
+		// }, // computed
+		// created() { 
+		// },
+		// mounted() { 
+		// },
+		methods: {
+			methodName() {
+				
 			}
-			// When enter from About page - END
-		},
+		} // methods
 	};
 </script>
 
 <template>
-	<div class="container">
-		<h1 class="my-5">
-			Shopping Cart <span v-if="cartContent">({{ cartContent }})</span>
-		</h1>
-		<div v-if="!cartContent">
-			<h3>Your Shopping Cart is empty.</h3>
-			<p>
-				Browse through our categories for inspiration and add something
-				you like.
-			</p>
-			<br />
-			<div class="d-grid gap-2 d-md-flex justify-content-md-start">
-				<button
-					class="btn btn-outline-dark me-md-2 px-md-5"
-					type="button"
-				>
-					NEW ARRIVALS
-				</button>
-				<button class="btn btn-outline-dark px-md-5" type="button">
-					TRENDING
-				</button>
-			</div>
-		</div>
-		<div class="cart-section">
-			<div class="row">
-				<div class="col-lg-8">
-					<CartItem
-						v-for="(item, index) in cart"
-						:key="index"
-						:name="item.name"
-						:price="item.price"
-						:size="item.size"
-						:brand="item.brand"
-						:seller="item.seller"
-						:imgUrl="item.imgUrl"
-						:class="cartItemStyle(index)"
-					/>
-				</div>
-				<div class="col-lg-4">
-					<CheckoutBar
-						:totalOriginal="cartTotal"
-						:total="cartTotal"
-						:itemCount="cart.length"
-					/>
-				</div>
-			</div>
-		</div>
-		<div class="section">
-			<h3>Recently Viewed</h3>
-		</div>
-		<div class="section">
-			<h3>Recommended For You</h3>
-		</div>
-	</div>
+	<body>
+		<h2 class="m-5 mb-4">Order History</h2>
+		<div class="container-fluid">
+			<PastOrders
+			v-for="(item, index) in items"
+			:key="index"
+			:deliverydate="item.deliverydate"
+			:imgUrl="item.imgUrl"
+			:brand="item.brand"
+			:size="item.size"
+			:seller="item.seller"
+			:name="item.name"
+			/>
+		</div>	
+	</body>
 </template>
 
+
+
 <style scoped>
-	.section {
-		margin-top: 50px;
-	}
+p {
+	color: black;
+}
+
+h2 {
+	font-family: 'inter-bold';
+	color: black;
+}
 </style>
