d2caf5510a76ed05b5a051959735019462086c5f
viqeey <fsantoso.2022@scis.smu.edu.sg>
2023-10-24 16:04:31 +0800

    order history almost complete, just need buttons

---
+++ src/components/PastOrder.vue
@@ -0,0 +1,105 @@
+<script>
+export default {
+    props: {
+        deliverydate: {
+            type: String,
+            required: true
+        },
+        imgUrl: {
+            type: String,
+            required: true,
+        },
+        brand: {
+            type: String,
+            required: false,
+        },
+        size: {
+            type: String,
+            required: true,
+        },
+        seller: {
+            type: String,
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+
+    },
+    events: [],
+    data() {
+        return {
+            // key: value
+        }
+    }, // data
+    
+    methods: {
+        methodName() {
+            
+        }
+    }, // methods;
+    
+    watch: {
+
+    },
+
+    computed: {
+
+    }
+    // component must be declared before app.mount(...)
+};
+</script>
+
+
+<template>
+    <div class="row m-1 p-5">
+        <div class="col col-auto justify-content-center">
+            <img :src="imgUrl" :alt="name" class="itemImg"/>
+        </div>
+        <div class="col col-auto justify-content-center">
+            <p class="deliveryDate">DELIVERED ON {{deliverydate}}</p>
+            <p class="itemName">{{name}}</p>
+            <p class="brandName">{{brand}}</p>
+            <p class="size">Size: {{size}}</p>
+            <p class="seller">Seller: {{seller}}</p>
+        </div>
+    </div>
+</template>
+
+<style>
+p {
+    font-size: 14px;
+}
+
+.deliveryDate {
+    font-family: 'inter-light';
+    font-style: italic;
+    text-transform: uppercase;
+    padding-top: 5px;
+}
+
+.itemName {
+    font-family: 'inter-bold';
+    font-size: 16px;
+}
+
+.brandName {
+    font-family: 'inter-regular';
+}
+
+.seller {
+    font-family: 'inter-regular';
+    text-decoration: underline;
+}
+
+.itemImg {
+    width: auto;
+    max-width: 167px;
+    max-height: 200px;
+    object-fit: cover;
+    float: left;
+    margin-right: 1rem;
+}
+
+</style>
---
+++ src/views/OrderHistory.vue
@@ -0,0 +1,64 @@
+<script>
+import PastOrders from "../components/PastOrder.vue";
+export default {
+	// <div id='app'></div>
+		components: {
+			PastOrders,
+		},
+		data() { 
+			return { 
+		// key: value
+			items: [
+				{ deliverydate: "2023/10/24", imgUrl: "/src/assets/img/ecommerce/placeholderImage.jpg", brand: "gucci", size: "L", seller: "Lay Foo", name: "Men's Jeans" },
+
+				{ deliverydate: "2023/10/22", imgUrl: '/src/assets/img/ecommerce/placeholderImage.jpg', brand: "gucci", size: "L", seller: "Lay Foo", name: "Men's Jeans" },
+			],
+			};
+		}, // data
+		// computed: { 
+		//     derivedProperty() {
+		//         return false;
+		//     }  
+		// }, // computed
+		// created() { 
+		// },
+		// mounted() { 
+		// },
+		methods: {
+			methodName() {
+				
+			}
+		} // methods
+	};
+</script>
+
+<template>
+	<body>
+		<h2 class="m-5 mb-4">Order History</h2>
+		<div class="container-fluid">
+			<PastOrders
+			v-for="(item, index) in items"
+			:key="index"
+			:deliverydate="item.deliverydate"
+			:imgUrl="item.imgUrl"
+			:brand="item.brand"
+			:size="item.size"
+			:seller="item.seller"
+			:name="item.name"
+			/>
+		</div>	
+	</body>
+</template>
+
+
+
+<style scoped>
+p {
+	color: black;
+}
+
+h2 {
+	font-family: 'inter-bold';
+	color: black;
+}
+</style>
