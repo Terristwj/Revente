7241fc1270d6a480c59f0466153a3439e4017771
0Ogw <glen.wang2709@gmail.com>
2023-10-26 16:24:04 +0800

    Standardised sidebar to use Prime Vue

--- src/components/SideMenuBar.vue
+++ src/components/SideMenuBar.vue
@@ -1,13 +1,6 @@
 <script>
 	export default {
 		name: 'SideMenuBar',
-		props: {
-			isSidebarFullscreen: {
-				type: Boolean,
-				required: false,
-				default: true,
-			},
-		},
 		data() {
 			return {
 				// Gender section
@@ -55,219 +48,178 @@
 				console.log(this.maxPriceInput);
 			},
 		},
-		computed: {
-			sidebarClass() {
-				return this.isSidebarFullscreen
-					? 'offcanvas offcanvas-start'
-					: '';
-			},
-			sidebarStyle() {
-				return this.isSidebarFullscreen ? '' : 'max-width: 260px';
-			},
-		},
 	};
 </script>
 
 <template>
-	<div
-		:class="sidebarClass"
-		tabindex="-1"
-		id="offcanvas"
-		data-bs-keyboard="false"
-		data-bs-backdrop="false"
-	>
-		<div class="offcanvas-header" v-if="isSidebarFullscreen">
-			<h6 class="offcanvas-title" id="offcanvas">Filter</h6>
-			<button
-				type="button"
-				class="btn-close text-reset"
-				data-bs-dismiss="offcanvas"
-				aria-label="Close"
-			></button>
-		</div>
-		<div class="flex-shrink-0 p-3 offcanvas-body" :style="sidebarStyle">
-			<!-- Gender (default open) -->
-			<ul class="list-unstyled ps-0">
-				<li class="mb-1">
-					<div class="row">
-						<div
-							class="col-10"
-							data-bs-toggle="collapse"
-							data-bs-target="#gender-collapse"
-							aria-expanded="true"
-						>
-							Gender
+	<!-- Gender (default open) -->
+	<ul class="list-unstyled ps-0">
+		<li class="mb-1">
+			<div class="row">
+				<div
+					class="col-10"
+					data-bs-toggle="collapse"
+					data-bs-target="#gender-collapse"
+					aria-expanded="true"
+				>
+					Gender
+				</div>
+				<button
+					class="col-2 btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed"
+					data-bs-toggle="collapse"
+					data-bs-target="#gender-collapse"
+					aria-expanded="true"
+				></button>
+			</div>
+			<div class="collapse show" id="gender-collapse">
+				<ul
+					class="btn-toggle-nav list-unstyled fw-normal pb-1 small mt-4 ms-3 ms-3"
+				>
+					<li v-for="(gender, index) in genders" :key="index">
+						<div class="form-check">
+							<input
+								v-model="genderPicked"
+								class="form-check-input"
+								type="radio"
+								:value="gender"
+								:id="gender"
+							/>
+							<label class="form-check-label" :for="gender">
+								{{ gender }}
+							</label>
 						</div>
-						<button
-							class="col-2 btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed"
-							data-bs-toggle="collapse"
-							data-bs-target="#gender-collapse"
-							aria-expanded="true"
-						></button>
-					</div>
-					<div class="collapse show" id="gender-collapse">
-						<ul
-							class="btn-toggle-nav list-unstyled fw-normal pb-1 small mt-4 ms-3 ms-3"
-						>
-							<li v-for="(gender, index) in genders" :key="index">
-								<div class="form-check">
-									<input
-										v-model="genderPicked"
-										class="form-check-input"
-										type="radio"
-										:value="gender"
-										:id="gender"
-									/>
-									<label
-										class="form-check-label"
-										:for="gender"
-									>
-										{{ gender }}
-									</label>
-								</div>
-							</li>
-						</ul>
-					</div>
-				</li>
+					</li>
+				</ul>
+			</div>
+		</li>
 
-				<li class="border-top my-3"></li>
-				<!-- Brand  -->
-				<li class="mb-1">
-					<div class="row">
-						<div
-							class="col-10"
-							data-bs-toggle="collapse"
-							data-bs-target="#brand-collapse"
-							aria-expanded="true"
-						>
-							Brand
+		<li class="border-top my-3"></li>
+		<!-- Brand  -->
+		<li class="mb-1">
+			<div class="row">
+				<div
+					class="col-10"
+					data-bs-toggle="collapse"
+					data-bs-target="#brand-collapse"
+					aria-expanded="true"
+				>
+					Brand
+				</div>
+				<button
+					class="col-2 btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed"
+					data-bs-toggle="collapse"
+					data-bs-target="#brand-collapse"
+					aria-expanded="true"
+				></button>
+			</div>
+			<div class="collapse show" id="brand-collapse">
+				<ul
+					class="btn-toggle-nav list-unstyled fw-normal pb-1 small mt-4 ms-3 ms-3"
+				>
+					<li>
+						<div class="input-group mb-3">
+							<input
+								type="text"
+								class="form-control"
+								placeholder="Search Brands"
+								aria-label="Search Brands"
+								v-model="brandSearchInput"
+							/>
 						</div>
-						<button
-							class="col-2 btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed"
-							data-bs-toggle="collapse"
-							data-bs-target="#brand-collapse"
-							aria-expanded="true"
-						></button>
-					</div>
-					<div class="collapse show" id="brand-collapse">
-						<ul
-							class="btn-toggle-nav list-unstyled fw-normal pb-1 small mt-4 ms-3 ms-3"
-						>
-							<li>
-								<div class="input-group mb-3">
-									<input
-										type="text"
-										class="form-control"
-										placeholder="Search Brands"
-										aria-label="Search Brands"
-										v-model="brandSearchInput"
-									/>
-								</div>
-							</li>
-							<li v-for="brand in brands" :key="brand">
-								<div class="form-check">
-									<input
-										class="form-check-input"
-										type="checkbox"
-										v-model="brandsPicked"
-										:value="brand"
-										:id="brand"
-									/>
-									<label
-										class="form-check-label"
-										:for="brand"
-									>
-										{{ brand }}
-									</label>
-								</div>
-							</li>
-						</ul>
-					</div>
-				</li>
+					</li>
+					<li v-for="brand in brands" :key="brand">
+						<div class="form-check">
+							<input
+								class="form-check-input"
+								type="checkbox"
+								v-model="brandsPicked"
+								:value="brand"
+								:id="brand"
+							/>
+							<label class="form-check-label" :for="brand">
+								{{ brand }}
+							</label>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</li>
 
-				<li class="border-top my-3"></li>
-				<!-- Price -->
-				<li class="mb-1">
-					<div class="row">
-						<div
-							class="col-10"
-							data-bs-toggle="collapse"
-							data-bs-target="#price-collapse"
-							aria-expanded="true"
-						>
-							Price
-						</div>
-						<button
-							class="col-2 btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed"
-							data-bs-toggle="collapse"
-							data-bs-target="#price-collapse"
-							aria-expanded="true"
-						></button>
+		<li class="border-top my-3"></li>
+		<!-- Price -->
+		<li class="mb-1">
+			<div class="row">
+				<div
+					class="col-10"
+					data-bs-toggle="collapse"
+					data-bs-target="#price-collapse"
+					aria-expanded="true"
+				>
+					Price
+				</div>
+				<button
+					class="col-2 btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed"
+					data-bs-toggle="collapse"
+					data-bs-target="#price-collapse"
+					aria-expanded="true"
+				></button>
+			</div>
+			<div class="collapse show" id="price-collapse">
+				<div
+					class="btn-toggle-nav list-unstyled fw-normal pb-1 small mt-4 ms-3"
+				>
+					<div
+						class="form-check"
+						v-for="priceRange in priceRanges"
+						:key="priceRange"
+					>
+						<input
+							v-model="priceRangeSelected"
+							class="form-check-input"
+							type="radio"
+							:id="priceRange"
+							:value="priceRange"
+						/>
+						<label class="form-check-label" :for="priceRange">
+							{{ priceRange }}
+						</label>
 					</div>
-					<div class="collapse show" id="price-collapse">
-						<div
-							class="btn-toggle-nav list-unstyled fw-normal pb-1 small mt-4 ms-3"
-						>
-							<div
-								class="form-check"
-								v-for="priceRange in priceRanges"
-								:key="priceRange"
-							>
+					<div class="mt-4">
+						<div class="row price-search">
+							<div class="col-4 pe-0">
 								<input
-									v-model="priceRangeSelected"
-									class="form-check-input"
-									type="radio"
-									:id="priceRange"
-									:value="priceRange"
+									type="number"
+									class="price-form"
+									placeholder="0"
+									v-model="minPriceInput"
+									min="0"
 								/>
-								<label
-									class="form-check-label"
-									:for="priceRange"
-								>
-									{{ priceRange }}
-								</label>
 							</div>
-							<div class="mt-4">
-								<div class="row price-search">
-									<div class="col-4 pe-0">
-										<input
-											type="number"
-											class="price-form"
-											placeholder="0"
-											v-model="minPriceInput"
-											min="0"
-										/>
-									</div>
-									<div class="dash col-1 py-0">-</div>
-									<div class="col-4 ps-0">
-										<input
-											type="number"
-											class="price-form"
-											placeholder="0"
-											v-model="maxPriceInput"
-											min="0"
-										/>
-									</div>
-									<div class="col-2 ps-0">
-										<button
-											class="price-btn"
-											@click="searchPrice"
-										>
-											Apply
-										</button>
-									</div>
-								</div>
-								<div v-if="isPriceRangeWrong">
-									<div class="text-danger mt-2" role="alert">
-										min price is greater than max price!
-									</div>
-								</div>
+							<div class="dash col-1 py-0">-</div>
+							<div class="col-4 ps-0">
+								<input
+									type="number"
+									class="price-form"
+									placeholder="0"
+									v-model="maxPriceInput"
+									min="0"
+								/>
+							</div>
+							<div class="col-2 ps-0">
+								<button class="price-btn" @click="searchPrice">
+									Apply
+								</button>
+							</div>
+						</div>
+						<div v-if="isPriceRangeWrong">
+							<div class="text-danger mt-2" role="alert">
+								min price is greater than max price!
 							</div>
 						</div>
 					</div>
-				</li>
-				<!-- Size -->
-			</ul>
-		</div>
-	</div>
+				</div>
+			</div>
+		</li>
+		<!-- Size -->
+	</ul>
 </template>
--- src/views/ListingView.vue
+++ src/views/ListingView.vue
@@ -17,38 +17,42 @@
 		data() {
 			return {
 				test: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+				visibleLeft: false,
 			};
-		},
-		computed: {
-			isSidebarFullscreen() {
-				return this.windowWidth < 576;
-			},
 		},
 	};
 </script>
 
 <template>
 	<body>
-		<div class="m-3">
-			<button
-				class="btn btn-outline-dark d-inline-flex align-items-center"
-				data-bs-toggle="offcanvas"
-				data-bs-target="#offcanvas"
-				role="button"
-				v-if="isSidebarFullscreen"
-			>
-				Filter
-				<font-awesome-icon :icon="['fab', 'filter']" />
-			</button>
-		</div>
 		<div class="container-fluid">
 			<div class="row">
-				<div class="col-auto">
-					<SideMenuBar :isSidebarFullscreen="isSidebarFullscreen" />
+				<div class="my-3">
+					<button
+						class="btn btn-outline-dark d-inline-flex align-items-center d-block d-lg-none"
+						data-bs-toggle="offcanvas"
+						data-bs-target="#offcanvas"
+						role="button"
+						@click="visibleLeft = true"
+					>
+						Filter
+						<font-awesome-icon :icon="['fab', 'filter']" />
+					</button>
 				</div>
-				<div class="col-9">
+			</div>
+			<div class="row">
+				<div class="col-lg-3 d-none d-lg-block">
+					<SideMenuBar />
+				</div>
+				<Sidebar
+					v-model:visible="visibleLeft"
+					class="d-block d-lg-none"
+				>
+					<SideMenuBar />
+				</Sidebar>
+				<div class="col-md-12 col-lg-9">
 					<div
-						class="row row-cols-2 row-cols-md-3 row-cols-lg-4 justify-content-center gap-1 gap-md-2 gap-lg-3"
+						class="row row-cols-2 justify-content-center gap-2 gap-lg-3"
 					>
 						<ItemCard v-for="(num, index) in test" :key="index" />
 					</div>
