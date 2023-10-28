dd2d7abcd3b9f5be303f6145bd016953c892b9e8
Owen Goh <owengoh2001@gmail.com>
2023-10-28 16:31:25 +0800

    Update SuccessView.vue

--- src/views/SuccessView.vue
+++ src/views/SuccessView.vue
@@ -1,12 +1,19 @@
 <template>
-    <Card>
-        <template #title> Simple Card </template>
-        <template #content>
-            <p class="m-0">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
-                quas!
-            </p>
-        </template>
-    </Card>
+    <div class="row justify-content-center">
+        <div class="col-6 p-3">
+        <Card>
+            <template #title> Success </template>
+            <template #content>
+                <p class="m-0">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
+                    quas!
+                </p>
+            </template>
+        </Card>
+        </div>
+        <div class="col-6 p-3">
+            <img src="" alt="">
+        </div>
+    </div>
 </template>
 
