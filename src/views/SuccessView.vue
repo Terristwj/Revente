e005aef5a56af097b7cfb54aead07ab142068d74
Owen Goh <owengoh2001@gmail.com>
2023-10-28 16:22:12 +0800

    Added success page

---
+++ src/views/SuccessView.vue
@@ -0,0 +1,12 @@
+<template>
+    <Card>
+        <template #title> Simple Card </template>
+        <template #content>
+            <p class="m-0">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
+                quas!
+            </p>
+        </template>
+    </Card>
+</template>
+
