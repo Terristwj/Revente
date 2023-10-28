9698e7fc538cdf972eb3e9bd47e176c4d6073621
Owen Goh <owengoh2001@gmail.com>
2023-10-28 21:29:05 +0800

    Added SuccessView

--- src/views/SuccessView.vue
+++ src/views/SuccessView.vue
@@ -1,19 +1,49 @@
 <template>
-    <div class="row justify-content-center">
-        <div class="col-6 p-3">
-        <Card>
-            <template #title> Success </template>
-            <template #content>
-                <p class="m-0">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
-                    quas!
-                </p>
-            </template>
-        </Card>
+    <div class="row justify-content-center align-items-center">
+        <div class="col-md-12 order-sm-last col-lg-6 p-3 text-center order-xs-last">
+            <Card>
+                <template #title> Successful Checkout! </template>
+                <template #content>
+                    <p class="m-0">
+                        Thanks for making a purchase with us! We hope you enjoy your new item(s).
+                    </p>
+                    <button type="button" class="btn btn-dark mt-4" @click="goToListing()">Back To Listings</button>
+                </template>
+            </Card>
         </div>
-        <div class="col-6 p-3">
-            <img src="" alt="">
+        <div class="col-md-8 col-lg-6 p-3 d-flex justify-content-center align-items-center order-lg-last">
+            <img class='img-fluid' src="../assets/img/ecommerce/success.png" alt="">
         </div>
     </div>
 </template>
 
+<style scoped>
+.img-fluid {
+    height: auto;
+    width: 100%;
+    padding: 5rem;
+}
+
+.btn {
+    border-radius: 23px;
+}
+
+.btn-dark:hover {
+    background-color: transparent;
+    border: 1px solid black;
+    color: black;
+}
+</style>
+
+<script>
+
+
+export default {
+    methods: {
+        goToListing() {
+            console.log("working");
+            this.$router.push({name: "Listing"});
+        },
+    }
+}
+</script>
