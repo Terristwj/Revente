ebf862c44e036dec591d57410e69e3febf593963
Owen Goh <owengoh2001@gmail.com>
2023-10-31 16:39:28 +0800

    Updated the OrderHistory to get the orders of buyer

--- src/views/OrderHistory.vue
+++ src/views/OrderHistory.vue
@@ -1,5 +1,9 @@
 <script>
 import PastOrders from "../components/PastOrder.vue";
+import FBInstanceFirestore from "../services/Firebase/FirestoreDatabase.js";
+import { userStore } from "../main.js";
+import { watchEffect } from "vue";
+
 export default {
     // <div id='app'></div>
     components: {
@@ -8,43 +12,45 @@ export default {
     data() {
         return {
             // key: value
-            items: [
-                {
-                    deliverydate: "2023/10/24",
-                    imgUrl: "/src/assets/img/ecommerce/placeholderImage.jpg",
-                    brand: "gucci",
-                    size: "L",
-                    seller: "Lay Foo",
-                    name: "Men's Jeans",
-                },
-
-                {
-                    deliverydate: "2023/10/22",
-                    imgUrl: "/src/assets/img/ecommerce/placeholderImage.jpg",
-                    brand: "gucci",
-                    size: "L",
-                    seller: "Lay Foo",
-                    name: "Men's Jeans",
-                },
-            ],
+            userID: "",
+            items: [],
         };
     },
     created() {
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
+        setTimeout(() => {
+            this.getItems(this.userID);
+        }, 1000);
     },
-    // data
-    // computed: {
-    //     derivedProperty() {
-    //         return false;
-    //     }
-    // }, // computed
-    // created() {
-    // },
-    // mounted() {
-    // },
+    mounted() {
+        watchEffect(() => {
+            this.userID = userStore.getUserID();
+        })
+    },
     methods: {
-        methodName() {},
+        getItems(userID) {
+            console.log(userID);
+            FBInstanceFirestore.getProductsBasedOnBuyerID(userID).then((data) => {
+                console.log(data);
+                data.forEach(item => {
+                    var obj = {}
+                    console.log(item.seller_ID);
+                    obj["brand"] = item.brand;
+                    obj["imgUrl"] = item.image_src;
+                    obj["name"] = item.product_name;
+                    obj["size"] = item.size;
+                    obj["seller_name"] = null;
+                    // this.items.push(item)
+                    FBInstanceFirestore.getUser(item.seller_ID).then((data) => {
+                        let sellerName = data.first_name + " " + data.last_name;
+                        console.log(sellerName);
+                        obj["seller_name"] = sellerName;
+                        this.items.push(obj);
+                    })
+                });
+            });
+        },
     }, // methods
 };
 </script>
@@ -56,16 +62,8 @@ export default {
 
         <!--BELOW ARE THE PAST ORDER ITEMS-->
         <div class="container-fluid">
-            <PastOrders
-                v-for="(item, index) in items"
-                :key="index"
-                :deliverydate="item.deliverydate"
-                :imgUrl="item.imgUrl"
-                :brand="item.brand"
-                :size="item.size"
-                :seller="item.seller"
-                :name="item.name"
-            />
+            <PastOrders v-for="(item, index) in items" :key="index" :deliverydate="item.deliverydate" :imgUrl="item.imgUrl"
+                :brand="item.brand" :size="item.size" :seller="item.seller_name" :name="item.name" />
         </div>
     </body>
 </template>
