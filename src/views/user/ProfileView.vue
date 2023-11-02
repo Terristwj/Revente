483573a50ea4c2dd4238de7ac4dc15dd110aadaa
Terris <27739101+Terristwj@users.noreply.github.com>
2023-11-02 11:48:20 +0800

    Update ProfileView.vue

--- src/views/user/ProfileView.vue
+++ src/views/user/ProfileView.vue
@@ -7,28 +7,26 @@ import SmallCarouselMainPage from "../../components/SmallCarouselMainPage.vue";
 import SmallCarouselRecents from "../../components/SmallCarouselRecents.vue";
 import vue3starRatings from "vue3-star-ratings";
 
-
 export default {
     data() {
         return {
             // need to dynamically set this rating based on average of all reviews
             total_rating: 0,
             rating: 0,
-            userID: userStore.getUserID(),
+            userID: "",
             description: "",
             image_src: "",
             username: "",
             items: [],
             reviews: [],
         };
-
     },
     computed: {
         haveListing() {
             return this.items.length > 4;
         },
-        littleListing(){
-        return this.items.length < 5;
+        littleListing() {
+            return this.items.length < 5;
         },
         noListings() {
             return this.items.length == 0;
@@ -45,23 +43,27 @@ export default {
         document.documentElement.scrollTop = 0;
     },
     mounted() {
-        setTimeout(() => {
-            if (!userStore.getUserID()) {
+        setTimeout(async () => {
+            this.userID = userStore.getUserID();
+
+            if (!this.userID) {
                 router.push("/login");
             } else {
                 // this.getItems(this.userID);
-                FBInstanceFirestore.getUser(this.userID).then((data) => {
+                await FBInstanceFirestore.getUser(this.userID).then((data) => {
                     this.username = data.first_name + " " + data.last_name;
                     this.description = data.description;
                     this.image_src = data.image_src;
-
                 });
                 // console.log(this.userID);
-                FBInstanceFirestore.getAllListedProductByUser(this.userID).then(
-                (data) => {
+                await FBInstanceFirestore.getAllListedProductByUser(
+                    this.userID
+                ).then((data) => {
                     this.items = data;
                 });
-                FBInstanceFirestore.getAllBoughtProductByUser(this.userID).then((data) => {
+                await FBInstanceFirestore.getAllBoughtProductByUser(
+                    this.userID
+                ).then((data) => {
                     data.forEach((item) => {
                         let obj = {};
                         obj["product_name"] = item.product_name;
@@ -71,24 +73,22 @@ export default {
                         obj["review_desc"] = item.review_desc;
                         this.total_rating += item.rating;
                         obj["rating"] = item.rating;
-                        FBInstanceFirestore.getUser(item.buyer_ID).then((data) => {
-                            let buyerName = data.first_name + " " + data.last_name;
-                            obj["buyer_name"] = buyerName;
-                            this.reviews.push(obj);
-                            this.rating = this.total_rating / this.reviews.length;
-                        });
-
-                        
-                    })
-                })
+                        FBInstanceFirestore.getUser(item.buyer_ID).then(
+                            (data) => {
+                                let buyerName =
+                                    data.first_name + " " + data.last_name;
+                                obj["buyer_name"] = buyerName;
+                                this.reviews.push(obj);
+                                this.rating =
+                                    this.total_rating / this.reviews.length;
+                            }
+                        );
+                    });
+                });
             }
-        }, 500);
-       
-
-
+        }, 1000);
     },
 };
-
 </script>
 
 <template>
@@ -98,13 +98,24 @@ export default {
             <div class="container-fluid py-2">
                 <div class="row">
                     <div class="col-xl-2 col-md-4 col-xs-6">
-                        <img :src="image_src" alt="generic profile picture" class="img-thumbnail mt-4 mb-2"
-                            style="width: 200px; z-index: 1" />
+                        <img
+                            :src="image_src"
+                            alt="generic profile picture"
+                            class="img-thumbnail mt-4 mb-2"
+                            style="width: 200px; z-index: 1"
+                        />
                     </div>
                     <div class="col-xl-10 col-md-8 col-xs-6">
                         <h1 class="display-5 fw-bold py-2">{{ username }}</h1>
                         Rating:
-                        <vue3starRatings v-model='this.rating' :starSize="'20'" starColor="#ff9800" inactiveColor="#333333" :numberOfStars="5" :disableClick="true"/>
+                        <vue3starRatings
+                            v-model="this.rating"
+                            :starSize="'20'"
+                            starColor="#ff9800"
+                            inactiveColor="#333333"
+                            :numberOfStars="5"
+                            :disableClick="true"
+                        />
                         <p class="col-md-12 fs-4 pt-2">
                             Description: {{ description }}
                         </p>
@@ -113,14 +124,18 @@ export default {
 
                 <!-- Listings -->
                 <div class="container-fluid" v-if="littleListing">
-                    <div class="row listings shadow-sm p-3 mb-5 bg-white rounded">
+                    <div
+                        class="row listings shadow-sm p-3 mb-5 bg-white rounded"
+                    >
                         <h2 class="text-center">Current Listings</h2>
                         <!-- add in using v-for -->
                         <SmallCarouselRecents :products="items" />
                     </div>
                 </div>
                 <div class="container-fluid" v-if="haveListing">
-                    <div class="row listings shadow-sm p-3 mb-5 bg-white rounded">
+                    <div
+                        class="row listings shadow-sm p-3 mb-5 bg-white rounded"
+                    >
                         <h2 class="text-center">Current Listings</h2>
                         <!-- add in using v-for -->
                         <SmallCarouselMainPage :products="items" />
@@ -128,7 +143,9 @@ export default {
                 </div>
 
                 <div class="container-fluid" v-if="noListings">
-                    <div class="row listings shadow-sm p-3 mb-5 bg-white rounded">
+                    <div
+                        class="row listings shadow-sm p-3 mb-5 bg-white rounded"
+                    >
                         <h2 class="text-center">Current Listings</h2>
                         <!-- add in using v-for -->
                         <p class="lead text-center">No items listed</p>
@@ -136,20 +153,37 @@ export default {
                 </div>
                 <!-- Reviews -->
                 <div class="container-fluid">
-                    <div class="row reviews shadow-sm p-3 mb-5 bg-white rounded">
+                    <div
+                        class="row reviews shadow-sm p-3 mb-5 bg-white rounded"
+                    >
                         <h2 class="text-center">Reviews</h2>
                         <!-- add in using v-for -->
-                        <div v-for="(item) in this.reviews" class='shadow-sm p-3 mb-5 bg-white rounded'>
+                        <div
+                            v-for="item in this.reviews"
+                            class="shadow-sm p-3 mb-5 bg-white rounded"
+                        >
                             <div class="row">
                                 <div class="col">
-                                    <vue3starRatings v-model='item.rating' :starSize="'15'" starColor="#ff9800" inactiveColor="#333333" :numberOfStars="5" :disableClick="true"/>
+                                    <vue3starRatings
+                                        v-model="item.rating"
+                                        :starSize="'15'"
+                                        starColor="#ff9800"
+                                        inactiveColor="#333333"
+                                        :numberOfStars="5"
+                                        :disableClick="true"
+                                    />
                                 </div>
                                 <div class="row">
                                     <div class="col">
-                                    <p class="lead">{{ item.product_name }}</p>
-                                    <p class="lead">{{ item.review_desc }}</p>
-                                    <p class="lead fw-bold">{{ item.buyer_name }}</p>
-
+                                        <p class="lead">
+                                            {{ item.product_name }}
+                                        </p>
+                                        <p class="lead">
+                                            {{ item.review_desc }}
+                                        </p>
+                                        <p class="lead fw-bold">
+                                            {{ item.buyer_name }}
+                                        </p>
                                     </div>
                                 </div>
                             </div>
