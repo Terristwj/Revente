17baae017e75b4af805fbaf1d2a2f3fee2189676
Terris <27739101+Terristwj@users.noreply.github.com>
2023-11-04 07:02:13 +0800

    Update chatbot UI

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import FooterComponent from "./components/FooterComponent.vue";
 export default {
     data() {
         return {
-            isOpen: false,
+            isChatOpen: false,
         };
     },
     components: {
@@ -14,10 +14,36 @@ export default {
     },
     methods: {
         toggleChatbot() {
-            this.isOpen = !this.isOpen;
+            this.isChatOpen = !this.isChatOpen;
+
+            let chatbot_button = document.getElementById("chatbot-button");
+            let chatbot = document.getElementById("chatbot");
+
+            if (this.isChatOpen) {
+                chatbot_button.style.right = "50px";
+                this.animateOpen(true, chatbot);
+            } else {
+                chatbot_button.style.right = "10px";
+                this.animateOpen(false, chatbot);
+            }
+        },
+        animateOpen(isChatOpen, chatbot) {
+            let id = null;
+
+            clearInterval(id);
+            id = setInterval(frame, 25);
+
+            // console.log(chatbot.style.width);
+            function frame() {
+                if (isChatOpen) {
+                    chatbot.style.width = "100%";
+                } else {
+                    chatbot.style.width = "0%";
+                }
+                clearInterval(id);
+            }
         },
     },
-
 };
 </script>
 
@@ -25,17 +51,17 @@ export default {
     <NavigationBar />
     <div id="content">
         <RouterView />
-        <div class="chatbot-container" v-if="isOpen">
-            <iframe allow="microphone;"
-                src="https://console.dialogflow.com/api-client/demo/embedded/ae2dede1-2cfa-4ba7-8458-062aaf47c01b"
-                class="chatbot"></iframe>
-        </div>
-
+    </div>
+    <div id="chatbot-container" v-show="isChatOpen">
+        <iframe
+            allow="microphone;"
+            src="https://console.dialogflow.com/api-client/demo/embedded/ae2dede1-2cfa-4ba7-8458-062aaf47c01b"
+            id="chatbot"
+        ></iframe>
     </div>
     <button id="chatbot-button" @click="toggleChatbot">
         <i class="fas fa-comment-alt"></i>
     </button>
-
 
     <FooterComponent />
 </template>
@@ -43,55 +69,52 @@ export default {
 <style scoped>
 #chatbot-button {
     position: fixed;
-    bottom: 20px;
-    right: 20px;
+    bottom: 10px;
+    right: 10px;
+
+    width: 40px;
+    height: 40px;
+
     background-color: #007bff;
+    text-align: center;
+    font-size: 16px;
     color: #fff;
+
     border: none;
     border-radius: 50%;
-    width: 50px;
-    height: 50px;
-    font-size: 24px;
+
     cursor: pointer;
-z-index: 999;
+    z-index: 999;
 }
 
-.chatbot-container {
+#chatbot-container {
     position: fixed;
     bottom: 0;
     right: 0;
+
+    width: 100%;
+    height: 100%;
+
     z-index: 2;
 }
 
-.chatbot {
-    border: none;
-    overflow: hidden;
-    margin: auto;
+#chatbot {
+    border: 1px solid black;
     display: block;
-    width: 25dvw;
-    height: 50dvh;
+    width: 100%;
+    height: 100%;
 }
 
-@media(max-width: 425px) {
-    .chatbot {
-        width: 75vw;
-        height: 50dvh;
-    }
-}
-
-@media(min-width:768px) {
-    .chatbot {
+@media (min-width: 768px) {
+    #chatbot-container {
         width: 50vw;
-        height: 50dvh;
+        height: 60vh;
     }
 }
 
-@media(min-width:1024px) {
-    .chatbot {
-        width: 25vw;
-        height: 50dvh;
+@media (min-width: 1024px) {
+    #chatbot-container {
+        width: 35vw;
     }
 }
-
-
 </style>
