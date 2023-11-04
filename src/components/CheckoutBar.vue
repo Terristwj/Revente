47b737aa9311896df951d5d02342c67f7ac763d6
0Ogw <glen.wang2709@gmail.com>
2023-11-04 18:55:32 +0800

    disable checkout when 0 item in cart

--- src/components/CheckoutBar.vue
+++ src/components/CheckoutBar.vue
@@ -1,104 +1,110 @@
 <script>
-import { userStore } from "../main.js";
+	import { userStore } from '../main.js';
 
-export default {
-	props: {
-		totalOriginal: {
-			type: Number,
-			required: true,
+	export default {
+		props: {
+			totalOriginal: {
+				type: Number,
+				required: true,
+			},
+			discounts: {
+				type: Number,
+				required: false,
+				default: 0,
+			},
+			shipping: {
+				type: Number,
+				required: false,
+				default: 0,
+			},
+			total: {
+				type: Number,
+				required: true,
+			},
+			itemCount: {
+				type: Number,
+				required: true,
+			},
+			cartItems: {
+				type: Array,
+				required: true,
+			},
+			itemIds: {
+				type: Array,
+				required: true,
+			},
 		},
-		discounts: {
-			type: Number,
-			required: false,
-			default: 0,
+		data() {
+			return {
+				promoInput: '',
+				promoMsg: '',
+				promoCodes: ['PROMO1', 'PROMO2', 'PROMO3'],
+				promoDetails: [],
+				userId: userStore.getUserID(),
+			};
 		},
-		shipping: {
-			type: Number,
-			required: false,
-			default: 0,
-		},
-		total: {
-			type: Number,
-			required: true,
-		},
-		itemCount: {
-			type: Number,
-			required: true,
-		},
-		cartItems: {
-			type: Array,
-			required: true,
-		},
-		itemIds: {
-			type: Array,
-			required: true,
-		}
-	},
-	data() {
-		return {
-			promoInput: '',
-			promoMsg: '',
-			// todo: replace with database promo codes later
-			promoCodes: ['PROMO1', 'PROMO2', 'PROMO3'],
-			promoDetails: [],
-			userId: userStore.getUserID(),
-		};
-	},
-	methods: {
-		goToCheckout() {
-			// console.log("checkout");
-			// console.log(this.itemIds);
-			console.log(this.userId);
-			fetch("https://revente-backend.vercel.app/create-checkout-session", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({
-					success_url: "http://localhost:5173/success?product_ids=" + (this.itemIds).toString() + "&user_id=" + this.userId,
-					cancel_url: "http://localhost:5173/cart",
-					items: this.cartItems,
-				}),
-			}).then(res => {
-				if (res.ok) return res.json()
-				return res.json().then(json => Promise.reject(json))
-			}).then(({ url }) => {
-				// console.log(url);
-				window.location = url
-			})
-				.catch(e => {
-					console.error(e.error)
-				})
-		},
-		applyPromo() {
-			this.promoMsg = '';
-			if (this.promoCodes.includes(this.promoInput)) {
-				this.promoMsg = 'Promo code applied!';
+		methods: {
+			goToCheckout() {
+				if (this.itemCount === 0) return;
 
-			} else {
-				this.promoMsg = 'Invalid promo code!';
-				setTimeout(() => {
-					this.promoMsg = '';
-				}, 4000);
-				this.promoInput = '';
-			}
+				fetch(
+					'https://revente-backend.vercel.app/create-checkout-session',
+					{
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json',
+						},
+						body: JSON.stringify({
+							success_url:
+								'http://localhost:5173/success?product_ids=' +
+								this.itemIds.toString() +
+								'&user_id=' +
+								this.userId,
+							cancel_url: 'http://localhost:5173/cart',
+							items: this.cartItems,
+						}),
+					}
+				)
+					.then((res) => {
+						if (res.ok) return res.json();
+						return res.json().then((json) => Promise.reject(json));
+					})
+					.then(({ url }) => {
+						// console.log(url);
+						window.location = url;
+					})
+					.catch((e) => {
+						console.error(e.error);
+					});
+			},
+			applyPromo() {
+				this.promoMsg = '';
+				if (this.promoCodes.includes(this.promoInput)) {
+					this.promoMsg = 'Promo code applied!';
+				} else {
+					this.promoMsg = 'Invalid promo code!';
+					setTimeout(() => {
+						this.promoMsg = '';
+					}, 4000);
+					this.promoInput = '';
+				}
+			},
 		},
-	},
-	computed: {
-		calculatedPromoClass() {
-			if (this.promoMsg === 'Invalid promo code!') {
-				return 'text-danger';
-			} else {
-				return 'text-success';
-			}
+		computed: {
+			calculatedPromoClass() {
+				if (this.promoMsg === 'Invalid promo code!') {
+					return 'text-danger';
+				} else {
+					return 'text-success';
+				}
+			},
 		},
-	},
-	watch: {
-		promoInput() {
-			this.promoInput = this.promoInput.toUpperCase();
+		watch: {
+			promoInput() {
+				this.promoInput = this.promoInput.toUpperCase();
+			},
 		},
-	},
-};
+	};
 </script>
 
 <template>
@@ -110,13 +116,27 @@ export default {
 		<div class="row">
 			<div class="col-sm-9">
 				<div class="input-group mb-3">
-					<input autofocus type="text" class="form-control" placeholder="Enter promo code here"
-						aria-label="promo code input" aria-describedby="promo-btn" id="promoCode" v-model="promoInput"
-						@keydown.enter="applyPromo" />
+					<input
+						autofocus
+						type="text"
+						class="form-control"
+						placeholder="Enter promo code here"
+						aria-label="promo code input"
+						aria-describedby="promo-btn"
+						id="promoCode"
+						v-model="promoInput"
+						@keydown.enter="applyPromo"
+					/>
 				</div>
 			</div>
 			<div class="col-sm-3">
-				<button class="btn btn-dark" type="button" id="promo-btn" @click="applyPromo">
+				<button
+					class="btn btn-dark"
+					type="button"
+					id="promo-btn"
+					:disabled="itemCount === 0"
+					@click="applyPromo"
+				>
 					Apply
 				</button>
 			</div>
@@ -129,7 +149,7 @@ export default {
 			<p>${{ totalOriginal }}</p>
 		</div>
 		<div class="d-flex justify-content-between">
-			<p>Discounts</p>
+			<p>Discount</p>
 			<p>${{ discounts }}</p>
 		</div>
 		<div class="d-flex justify-content-between">
@@ -142,7 +162,11 @@ export default {
 		<hr class="my-5" />
 		<!-- Proceed to Checkout -->
 		<div class="row mx-1">
-			<button class="btn btn-dark" @click="goToCheckout">
+			<button
+				class="btn btn-dark"
+				@click="goToCheckout"
+				:disabled="itemCount === 0"
+			>
 				PROCEED TO CHECKOUT
 			</button>
 		</div>
@@ -150,11 +174,11 @@ export default {
 </template>
 
 <style scoped>
-p {
-	line-height: 70%;
-}
+	p {
+		line-height: 70%;
+	}
 
-.border-red {
-	border: 2px solid red;
-}
+	.border-red {
+		border: 2px solid red;
+	}
 </style>
